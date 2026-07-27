0ac6ffc1144bf396756dac9232103b4f317d4f73
ayan khan <ayankhanayan992@gmail.com>
2026-07-27 14:36:07 +0530

    day -03

--- DAY 03/class2/src/components/Navbar.jsx
+++ DAY 03/class2/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='nav'>
-        
+        <h3>home</h3>
+        <h3>contact</h3>
+        <h3>design</h3>
     </div>
   )
 }
