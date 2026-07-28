ad98e9ae732406bd5914a70add6b12b380cf35db
ayan khan <ayankhanayan992@gmail.com>
2026-07-28 16:14:16 +0530

    intro to tailwind

---
+++ DAY 04/class3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+        <h2 className='text-7xl font-bold bg-red-500' >hlww ayan </h2>
+        <h3 className='bg-red-100'>ayaan</h3>
+    </div>
+  )
+}
+
+export default App
