28b4943181196cb411cad3719d55b45b72c25e38
ayan khan <ayankhanayan992@gmail.com>
2026-08-15 17:09:03 +0530

    Budget tracker

--- tracker/src/App.jsx
+++ tracker/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <h1>ap</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
